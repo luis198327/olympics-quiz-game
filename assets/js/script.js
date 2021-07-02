266e62d9ef853bf7c16581cbd5eb0d6e3e352558
luis198327 <luis.brites@sky.com>
2021-07-02 17:23:05 +0000

    Add initial variables and 1 question to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("quiz time");
+let question = document.getElementById("question");
+let answers = Array.from(document.getElementsByClassName("answer"));
+let questionMaximum = 10; //number of questions quiz will ask
+let currentQuestion = {}; //current question
+let score = 0; //score counter
+let questionNumber = 0; //question number quiz is on
+let questionLibrary = [ //questions are stored here
+        {
+            question: "The Olympic Games in Tokyo, Japan 2021 will officially be the 32nd Olympiad. But in what year did Tokyo last host the summer games?",
+            answer1: "1960",
+            answer2: "1964",
+            answer3: "1968",
+            answer4: "1972",
+            correct: "2"
+        },]
+
+function playGame() {
+    
+}
