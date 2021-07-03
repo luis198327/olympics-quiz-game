670682e4333597586a3b74e9a462bc9775a49816
luis198327 <luis.brites@sky.com>
2021-07-03 12:48:33 +0000

    Add favicon and block js to debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-let question = document.getElementById("question");
-let answers = Array.from(document.getElementsByClassName("answer"));
-let questionMaximum = 10; //number of questions quiz will ask
+const question = document.getElementById("question");
+const answers = Array.from(document.getElementsByClassName("answer"));
+console.log(answers);
+/*const questionMaximum = 10; //number of questions quiz will ask
 let currentQuestion = {}; //current question
 let score = 0; //score counter
 let questionNumber = 0; //question number quiz is on
+let unusedQuestions = []; //will obtain a new question for user
 let questionLibrary = [ //questions are stored here
         {
             question: "The Olympic Games in Tokyo, Japan 2021 will officially be the 32nd Olympiad. But in what year did Tokyo last host the summer games?",
@@ -14,6 +16,25 @@ let questionLibrary = [ //questions are stored here
             correct: "2"
         },]
 
+/**
+ * New game
+ *//*
 function playGame() {
-    
+    questionNumber = 0;
+    score = 0;
+    unusedQuestions = [...questionLibrary]  //uses full copy of the question library
+    newQuestion();
 }
+
+/**
+ * Obtains new question from question library
+ *//*
+function newQuestion() {
+    questionNumber++;
+    const questionIndex = Math.floor(Math.random() * unusedQuestions.length);   
+    currentQuestion = unusedQuestions[questionIndex];
+    question.innertext = currentQuestion.question;
+};
+
+
+playGame();*/
