b0412a7eadbba2192c327fc93bd12210a6c54954
luis198327 <luis.brites@sky.com>
2021-07-03 17:22:42 +0000

    Add for loop for answer options to the newquestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,10 +31,15 @@ function playGame() {
  * Obtains new question from question library
  */
 function newQuestion() {
-    questionNumber++;
-    const questionLibIndex = Math.floor(Math.random() * unusedQuestions.length);   
-    currentQuestion = unusedQuestions[questionLibIndex];
-    question.innerHTML = currentQuestion.question;
-};
+    questionNumber++;  //increments question number
+    const questionLibIndex = Math.floor(Math.random() * unusedQuestions.length); //to obtain random question
+    currentQuestion = unusedQuestions[questionLibIndex];  //ref to question
+    question.innerHTML = currentQuestion.question;  //set HTML to the question property
+  
+    //for loop to get answer options of each question and applies to A, B, C, D options
+    for (const answer of answers) {     
+        const number = answer.dataset['answer'];
+        answer.innerHTML = currentQuestion['answer' + number];
+    }}
 
-playGame();
+    playGame();
