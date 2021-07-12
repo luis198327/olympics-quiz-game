dda56536a9f5f7c4d84d30aa249186342dbd385d
luis198327 <luis.brites@sky.com>
2021-07-12 22:00:14 +0000

    Add initial if function to change background image on finish.html

--- assets/js/finish.js
+++ assets/js/finish.js
@@ -2,4 +2,14 @@ const finish = document.getElementById("final");
     const currentScore = localStorage.getItem("currentScore"); //pulls in score from local storage
 
     finish.innerHTML = currentScore; //replaces the element with the score from local storage
-    
+
+let body = document.getElementsByTagName('body')[0];
+
+/**
+ * function to change background image based on score
+ */
+    if(currentScore >= 9) {
+        body.style.backgroundImage = 'url("../assets/images/medal-1.jpg")';
+        body.style.backgroundRepeat = "no-repeat";
+        body.style.backgroundSize = "contain";
+    };
